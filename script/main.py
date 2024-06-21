41112f3d146b70bbe3118ad6d3082ad2f5d8e6f6
Varun Kumar <varuntheace@gmail.com>
2024-06-21 09:51:33 +0530

    updated shooting

--- script/main.py
+++ script/main.py
@@ -25,9 +25,11 @@ class Hero_ship(pygame.sprite.Sprite):
         self.rect.center = [x, y]
         self.health_start = health
         self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         speed = 8
+        cooldown = 0
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
@@ -35,15 +37,18 @@ class Hero_ship(pygame.sprite.Sprite):
         elif key[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
             self.rect.x += speed
         time_now = pygame.time.get_ticks()
-        if key[pygame.K_SPACE]:
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
             bullet = Bullets(self.rect.centerx, self.rect.top)
             bullet_group.add(bullet)
+            self.last_shot = time_now
         #draw health bar
         pygame.draw.rect(screen, 'red', (self.rect.x, (self.rect.bottom - 10), self.rect.width, 15))
         if self.health_remaining > 0:
             pygame.draw.rect(screen, 'green', (
-            self.rect.x, (self.rect.bottom - 10), int(self.rect.width * (self.health_remaining / self.health_start)),
-            15))
+                self.rect.x, (self.rect.bottom - 10),
+                int(self.rect.width * (self.health_remaining / self.health_start)),
+                15))
+
 
 class Bullets(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -55,6 +60,19 @@ class Bullets(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+class Aliens(pygame.sprite.Sprite):
+    def __init__(self, x, y, health):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('../assets/enemy.png')
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaining = health
 
 
 hero_group = pygame.sprite.Group()
