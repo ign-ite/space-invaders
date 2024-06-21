d52bcef885ab18e4dcadce4ce82b39e2aa408309
Varun Kumar <varuntheace@gmail.com>
2024-06-21 08:52:24 +0530

    ship can shoot!!

--- script/hero.py
+++ script/hero.py
@@ -1,12 +1,7 @@
 import pygame
 from shoot import Bullet
 
-class Hero(object):
-    def __init__(self):
-        self.image = pygame.image.load('../assets/ship.png')
-        self.image = pygame.transform.scale(self.image, (75, 75))
-        self.x = 270
-        self.y = 540
+
 
     def handle_keys(self, surface):
         keys = pygame.key.get_pressed()
@@ -15,11 +10,6 @@ class Hero(object):
             self.x -= dist
         elif keys[pygame.K_RIGHT] and self.x < 525:
             self.x += dist
-        if keys[pygame.K_UP]:
-            self.shoot(surface)
-    def shoot(self, surface):
-        bullet = Bullet(self.x, self.y)
-        bullet.move(surface)
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
--- script/main.py
+++ script/main.py
@@ -1,35 +1,94 @@
 import pygame
-from pygame import *
-from hero import Hero
+from pygame.locals import *
 from enemy import Enemy
 from random import randint
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
 pygame.init()
-screen = display.set_mode((600, 600))
-clock = time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Space Invaders')
+bg = pygame.image.load('../assets/background.png')
+
+
+def draw_bg():
+    screen.blit(bg, (0, 0))
+
+
+class Hero_ship(pygame.sprite.Sprite):
+    def __init__(self, x, y, health):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('../assets/ship.png')
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaining = health
+
+    def update(self):
+        speed = 8
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= speed
+        elif key[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+            self.rect.x += speed
+        time_now = pygame.time.get_ticks()
+        if key[pygame.K_SPACE]:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+        #draw health bar
+        pygame.draw.rect(screen, 'red', (self.rect.x, (self.rect.bottom - 10), self.rect.width, 15))
+        if self.health_remaining > 0:
+            pygame.draw.rect(screen, 'green', (
+            self.rect.x, (self.rect.bottom - 10), int(self.rect.width * (self.health_remaining / self.health_start)),
+            15))
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('../assets/bullet2.png')
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y -= 5
+
+
+hero_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+hero_ship = Hero_ship(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT - 70), 3)
+hero_group.add(hero_ship)
+
+clock = pygame.time.Clock()
 FPS = 40
 run = True
-hero = Hero()
-enemy_locations = [(5, 10),(65, 10), (125, 10), (185, 10), (245, 10), (305, 10), (365, 10), (425, 10), (485, 10), (545, 10),
-                   (5, 70),(65, 70), (125, 70), (185, 70), (245, 70), (305, 70), (365, 70), (425, 70), (485, 70), (545, 70),
-                   (5, 130),(65, 130), (125, 130), (185, 130), (245, 130), (305, 130), (365, 130), (425, 130), (485, 130), (545, 130)]
+
+enemy_locations = [(5, 10), (65, 10), (125, 10), (185, 10), (245, 10), (305, 10), (365, 10), (425, 10), (485, 10),
+                   (545, 10),
+                   (5, 70), (65, 70), (125, 70), (185, 70), (245, 70), (305, 70), (365, 70), (425, 70), (485, 70),
+                   (545, 70),
+                   (5, 130), (65, 130), (125, 130), (185, 130), (245, 130), (305, 130), (365, 130), (425, 130),
+                   (485, 130), (545, 130)]
 ENEMIES = []
 for pos in enemy_locations:
     enemy = Enemy(pos)
     ENEMIES.append(enemy)
 while run:
+    clock.tick(FPS)
+    draw_bg()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
 
-    hero.handle_keys(screen)
-
-    screen.fill('black')
-    hero.draw(screen)
-    ENEMIES[randint(0, len(ENEMIES) - 1)].drop(screen)
-
-
-
+    hero_ship.update()
+    bullet_group.update()
+    #update sprite groups
+    hero_group.draw(screen)
+    bullet_group.draw(screen)
     pygame.display.update()
-    clock.tick(FPS)
 pygame.quit()
